08ac8be79b62acf666ef50a5bf4681df446aed3a
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-02-12 22:38:45 +0900

    Add DelForm and AddForm component

--- memo-app/src/App.js
+++ memo-app/src/App.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Memo from './memo/Memo';
 import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+import DelForm from './memo/DelForm';
 
 
 class App extends Component {
@@ -21,6 +23,13 @@ class App extends Component {
         <h1>Memo</h1>
         <AddForm />
         <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}><DelForm /></td>
+            </tr>
+          </tbody>
+        </table>
         <Memo />
       </div>
     );
